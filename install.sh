f9f59112027069c268f967c773064a3634e5985f
itsmeodx <oussama15ouaadich@gmail.com>
2024-11-23 21:17:24 +0100

    Suppress output during repository cloning and make execution in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # clone the repository
-cd /tmp && git clone git@github.com:itsmeodx/ClassFileCreator.git && cd ClassFileCreator
+cd /tmp && git clone git@github.com:itsmeodx/ClassFileCreator.git 2>&1 > /dev/null
+cd ClassFileCreator
 
 # Make the executable
 make 2>&1 > /dev/null
