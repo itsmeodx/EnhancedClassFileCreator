e9481e71fb52017de5b985c1608da1e8112a1149
itsmeodx <oussama15ouaadich@gmail.com>
2024-11-23 21:38:20 +0100

    Refactor install script to handle repository cloning more robustly

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# clone the repository
-cd /tmp && git clone https://github.com/itsmeodx/ClassFileCreator.git 2>&1 > /dev/null
+# Clone the repository
+clone() {
+	git clone https://github.com/itsmeodx/ClassFileCreator.git 2> /dev/null > /dev/null
+}
+
+# Change to the temporary directory
+cd /tmp
+
+# Check if the repository is already cloned or not
+echo "Cloning the repository..."
+clone  || (rm -rf ClassFileCreator && clone)
+
+# Change to the repository directory
 cd ClassFileCreator
 
 # Make the executable
-make 2>&1 > /dev/null
+make
